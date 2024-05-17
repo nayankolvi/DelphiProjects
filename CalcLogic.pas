fe3e4be3ef8623a498d9a1e72ca2df2a32e5c9be
Uwe Raabe <uwe@uweraabe.de>
2024-05-17 15:40:43 +0200

    Changes from Session 1

--- CalcLogic.pas
+++ CalcLogic.pas
@@ -6,20 +6,66 @@ uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, System.Generics.Collections;
 
 type
-  CalculatorLogic = class
-    private
-
-    public
-      function PerformOperation (op1: Extended; op2 : Extended; op : Char): Extended;
-      function CalculateResult(Data : TList<string>): string;
-      function CheckIfOperatorFound(Data : TList<string>; Op : Char): Integer;
-
-    end;
+  TCalculator = class(TList<string>)
+  private
+    function GetOperatorIndex(Op: Char): Integer;
+    function PerformOperation(op1: Extended; op2 : Extended; op : Char): Extended;
+  public
+    function CalculateResult: string;
+  end;
 
 implementation
 
+function TCalculator.CalculateResult: string;
+const
+//  OpArray: Array[0..3] of Char = ('/', '*', '+', '-');
+  OpArray: TArray<Char> = ['/', '*', '+', '-'];
+var
+  OpIndex :Integer;
+  OpResult :string;
+
+begin
+  // use "BODMAS" rules to perform operation
+  // Intially go from right to left replace all the operations of the same
+  // todo : Handle exceptions
+  if Count = 0 then
+    begin
+      Result := '';
+      Exit;
+    end
+  else
+    begin
+      for var Op in OpArray do
+        begin
+          OpIndex :=  GetOperatorIndex(Op);
+          while OpIndex <> -1 do
+          begin
+            OpResult := FloatToStr(performOperation(StrToFloat(Items[OpIndex-1]), StrToFloat(Items[OpIndex+1]), Op));
+            if Count > 3 then
+              begin
+                DeleteRange(OpIndex-1, 3);
+                Insert(OpIndex-1, OpResult);
+              end
+            else
+              begin
+                DeleteRange(OpIndex-1, 3);
+                Insert(OpIndex-1, OpResult);
+                Result := OpResult;
+                Exit;
+              end;
+            OpIndex :=  GetOperatorIndex(Op);
+          end;
+        end;
+    end;
+end;
+
+function TCalculator.GetOperatorIndex(Op: Char): Integer;
+begin
+  Result := IndexOf(Op);
+end;
+
 // do the basic operations on the operands
-function CalculatorLogic.PerformOperation (op1: Extended; op2 : Extended; op : Char): Extended;
+function TCalculator.PerformOperation(op1: Extended; op2 : Extended; op : Char): Extended;
 begin
   case op of
   '+': Result := op1 + op2;
@@ -27,58 +73,6 @@ begin
   '*': Result := op1 * op2;
   '/': Result := op1 / op2;
   end;
-end;
-
-
-
-function CalculatorLogic.CalculateResult(Data : TList<string>): string;
-var
-  OpId, OpIndexInArray :Integer;
-  OpResult :string;
-  OpArray: array of Char;
-
-begin
-  OpArray := ['/', '*', '+', '-'];
-  // use "BODMAS" rules to perform operation
-  // Intially go from right to left replace all the operations of the same
-  // todo : Handle exceptions
-  if Data.Count = 0 then
-    begin
-      Result := '';
-      Exit;
-    end
-  else
-    begin
-      for OpId := Low(OpArray) to High(OpArray) do
-        begin
-          OpIndexInArray :=  CheckIfOperatorFound(Data, OpArray[OpId]);
-          while OpIndexInArray <> -1 do
-          begin
-            OpResult := FloatToStr(performOperation(StrToFloat(Data[OpIndexInArray-1]), StrToFloat(Data[OpIndexInArray+1]), OpArray[OpId]));
-            if Data.Count > 3 then
-              begin
-                Data.DeleteRange(OpIndexInArray-1, 3);
-                Data.Insert(OpIndexInArray-1, OpResult);
-              end
-            else
-              begin
-                Data.DeleteRange(OpIndexInArray-1, 3);
-                Data.Insert(OpIndexInArray-1, OpResult);
-                Result := OpResult;
-                Exit;
-              end;
-            OpIndexInArray :=  CheckIfOperatorFound(Data, OpArray[OpId]);
-          end;
-        end;
-    end;
-end;
-
-
-
-
-function CalculatorLogic.CheckIfOperatorFound(Data : TList<string>; Op : Char): Integer;
-begin
-  Result := Data.IndexOf(Op);
 end;
 
 end.
--- DisplayPanel.pas
+++ DisplayPanel.pas
@@ -10,11 +10,10 @@ type
   TDisplay = class(TFrame)
     DisplayTab: TEdit;
   private
-    { Private declarations }
-  public
-    { Public declarations }
-    procedure SetDisplayText(data : string);
     function GetDisplayText(): string;
+    procedure SetDisplayText(data : string);
+  public
+    property DisplayText: string read GetDisplayText write SetDisplayText;
   end;
 
 implementation
